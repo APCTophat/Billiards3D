80d02683be53f480e261c107ad776645bb34b471
APCTophat <49924900+APCTophat@users.noreply.github.com>
2019-04-29 11:55:25 +0200

    Added base

---
+++ Assignment3D-Billiards/Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hdirect = Input.GetAxis("Horizontal");
+        float Vdirect = Input.GetAxis("Vertical");
+
+  
+
+       
+        
+            rb.AddForce(Hdirect * 500, rb.velocity.y, Vdirect * 500);
+        
+        
+    }
+}
